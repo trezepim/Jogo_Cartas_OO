931cb46438f4afd1aa3daf5ff9dc6ddaff278d81
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-10-26 20:37:25 -0300

    feat: classe Jogador

---
+++ Jogo_OO/src/classes/Jogador.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Jogador {
+    private int vida;
+    private int defesa;
+
+    public Jogador(int vida, int defesa) {
+        this.vida = 2;
+        this.defesa = 50;
+    }
+}
